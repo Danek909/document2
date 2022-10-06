642c45545d0da1de24587f7471bc31051133802b
Danya <kogdana@gmail.com>
2022-10-06 16:31:21 +0300

    f

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$('.slider').slick({
+    infinite: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    arrows:true,
+    dots:true,
+    speed:1000,
+  });
+});
