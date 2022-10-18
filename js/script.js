bc81c014dea57bb9b5e50ed5f1dd3120b6ae42c6
Danya <kogdana@gmail.com>
2022-10-18 10:10:01 +0300

    hello

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
   $('.slider_2').slick({
       infinite: true,
       slidesToScroll: 1,
-      slidesToShow: 3,
+      slidesToShow: 4,
       arrows:true,
       speed:1000,
     });
@@ -23,8 +23,80 @@ $(document).ready(function(){
     $('.sliderBottom').slick({
         infinite: true,
         slidesToScroll: 1,
-        slidesToShow: 3,
+        slidesToShow: 4,
         arrows:true,
         speed:1000,
       });
     });
+
+    const factory = document.getElementById('button');
+    if(factory){
+        const isHeder = document.getElementById('ultext');
+        factory.addEventListener("click", function(e){
+            isHeder.classList.toggle('_activee');
+        }); 
+    }
+
+    const deletet = document.getElementById('button');
+    if(factory){
+        const isHeder = document.getElementById('buttonClik');
+        factory.addEventListener("click", function(e){
+            isHeder.classList.toggle('_activee');
+        }); 
+    }
+
+    const commit = document.getElementById('button');
+    if(factory){
+        const isHeder = document.getElementById('texLogin');
+        factory.addEventListener("click", function(e){
+            isHeder.classList.toggle('_activee');
+        }); 
+    }
+
+    const header = document.getElementById('button');
+    if(factory){
+        const isHeder = document.getElementById('heder');
+        factory.addEventListener("click", function(e){
+            isHeder.classList.toggle('_activee');
+        }); 
+    }
+
+    const text = document.getElementById('button');
+    if(factory){
+        const isHeder = document.getElementById('text');
+        factory.addEventListener("click", function(e){
+            isHeder.classList.toggle('_activee');
+        }); 
+    }
+
+    const spantext = document.getElementById('button');
+    if(factory){
+        const isHeder = document.getElementById('spantext');
+        factory.addEventListener("click", function(e){
+            isHeder.classList.toggle('_activee');
+        }); 
+    }
+
+    const button = document.getElementById('button');
+    if(factory){
+        const isHeder = document.getElementById('button');
+        factory.addEventListener("click", function(e){
+            isHeder.classList.toggle('_activee');
+        }); 
+    }
+
+    const burgerspan = document.getElementById('button');
+    if(factory){
+        const isHeder = document.getElementById('burgerSpan');
+        factory.addEventListener("click", function(e){
+            isHeder.classList.toggle('_activee');
+        }); 
+    }
+
+    const heder = document.getElementById('button');
+    if(factory){
+        const isHeder = document.getElementById('divHeder');
+        factory.addEventListener("click", function(e){
+            isHeder.classList.toggle('_activee');
+        }); 
+    }
